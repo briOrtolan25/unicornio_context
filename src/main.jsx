62c54a95bed8c6e6ca345500a02be1c8835f19c9
TobiasGentile <t.gentile@itecriocuarto.org.ar>
2025-04-08 20:51:41 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,24 @@
-import { StrictMode } from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App.jsx';
+import Unicorns from './unicorns/index.jsx';
+
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/unicornios" element={<Unicorns />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
